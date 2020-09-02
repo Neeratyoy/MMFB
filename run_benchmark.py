cca89e3be09dc8b7bee42c5ca4654b5701563913
neeratyoy <neeratyoy@gmail.com>
2020-09-02 19:55:22 +0200

    Serializing evaluations in a size 3 tuple format

--- run_benchmark.py
+++ run_benchmark.py
@@ -11,66 +11,116 @@ if __name__ == "__main__":
         3,       # kr-vs-kp
         # 168335,  # MiniBooNE   ### why failed???
         # 168331,  # volkert
-        # 31,      # credit-g
+        31,      # credit-g
         # 9977     # nomao
     ]
 
     seed = np.random.randint(1, 1000)
     n_configs = 5
     fidelity_space_granularity = 5
-
     results = {}
 
+    benchmark = RandomForestBenchmark(task_id=task_ids[0], seed=seed)
+
+    # Create list of configs
+    configs = benchmark.get_config(size=n_configs)
+
+    # Create list of fidelities
+    fidelity_grid = []
+    eps = 1e-10
+    for i, parameter in enumerate(benchmark.f_cs.get_hyperparameters()):
+        if isinstance(parameter, CS.UniformIntegerHyperparameter) and \
+                (parameter.upper - parameter.lower) < fidelity_space_granularity:
+            step = 1
+        else:
+            step = (parameter.upper - parameter.lower) / \
+                   (fidelity_space_granularity - 1)
+
+        grid_points = np.arange(
+            start=parameter.lower, stop=parameter.upper + eps, step=step
+        )
+
+        if isinstance(parameter, CS.UniformIntegerHyperparameter):
+            grid_points = np.ceil(grid_points).astype(int)
+
+        grid_points = np.clip(grid_points, a_min=None, a_max=parameter.upper)
+        fidelity_grid.append(grid_points)
+    fidelity_grid = list(itertools.product(*fidelity_grid))
+    fidelities = []
+    for i, fidelity in enumerate(fidelity_grid):
+        dummy_fidelity = benchmark.f_cs.sample_configuration()
+        config = fidelity[-1]
+        for j, parameter in enumerate(benchmark.f_cs.get_hyperparameters()):
+            dummy_fidelity[parameter.name] = fidelity[j]
+        fidelities.append(dummy_fidelity)
+
+    # all combinations of evaluations to be made
+    evaluations = list(itertools.product(*([task_ids, configs, fidelities])))
+
     start = time.time()
-    for task_id in task_ids:
+    for i in range(len(evaluations)):
+        print("Evaluating {} out of {}".format(
+            i + 1, len(task_ids) * n_configs * fidelity_space_granularity ** 2
+        ), end='\r')
+        task_id, config, fidelity = evaluations[i]
         benchmark = RandomForestBenchmark(task_id=task_id, seed=seed)
         benchmark.load_data_automl()
-        fidelity_grid = []
-        eps = 1e-10
-        results[task_id] = []
-        print("Beginning task_id={}\n{}\n".format(task_id, '=' * 25))
+        if task_id not in results.keys():
+            results[task_id] = []
+        results[task_id].append(benchmark.objective(config, fidelity))
 
-        # Create list of configs
-        configs = benchmark.get_config(size=n_configs)
+    print("Time taken since beginning: {:<.5f} seconds".format(time.time() - start))
 
-        # Create list of fidelities
-        for i, parameter in enumerate(benchmark.f_cs.get_hyperparameters()):
-            if isinstance(parameter, CS.UniformIntegerHyperparameter) and \
-                    (parameter.upper - parameter.lower) < fidelity_space_granularity:
-                step = 1
-            else:
-                step = (parameter.upper - parameter.lower) / \
-                       (fidelity_space_granularity - 1)
-
-            grid_points = np.arange(
-                start=parameter.lower, stop=parameter.upper + eps, step=step
-            )
-
-            if isinstance(parameter, CS.UniformIntegerHyperparameter):
-                grid_points = np.ceil(grid_points).astype(int)
-
-            grid_points = np.clip(grid_points, a_min=None, a_max=parameter.upper)
-            fidelity_grid.append(grid_points)
-        fidelity_grid = list(itertools.product(*fidelity_grid))
-        fidelities = []
-        for i, fidelity in enumerate(fidelity_grid):
-            dummy_fidelity = benchmark.f_cs.sample_configuration()
-            config = fidelity[-1]
-            for j, parameter in enumerate(benchmark.f_cs.get_hyperparameters()):
-                dummy_fidelity[parameter.name] = fidelity[j]
-            fidelities.append(dummy_fidelity)
-
-        # all combinations of evaluations to be made
-        evaluations = list(itertools.product(*([configs, fidelities])))
-
-        for i in range(len(evaluations)):
-            print("Evaluating {} out of {}".format(
-                i + 1, n_configs * fidelity_space_granularity ** 2
-            ), end='\r')
-            config, fidelity = evaluations[i]
-            results[task_id].append(benchmark.objective(config, fidelity))
-
-        print(results[task_id][0])
-        print(results[task_id][-1])
-        print()
-        print("Time taken since beginning: {:<.5f} seconds".format(time.time() - start))
+    # start = time.time()
+    # for task_id in task_ids:
+    #     benchmark = RandomForestBenchmark(task_id=task_id, seed=seed)
+    #     benchmark.load_data_automl()
+    #     fidelity_grid = []
+    #     eps = 1e-10
+    #     results[task_id] = []
+    #     print("Beginning task_id={}\n{}\n".format(task_id, '=' * 25))
+    #
+    #     # Create list of configs
+    #     configs = benchmark.get_config(size=n_configs)
+    #
+    #     # Create list of fidelities
+    #     for i, parameter in enumerate(benchmark.f_cs.get_hyperparameters()):
+    #         if isinstance(parameter, CS.UniformIntegerHyperparameter) and \
+    #                 (parameter.upper - parameter.lower) < fidelity_space_granularity:
+    #             step = 1
+    #         else:
+    #             step = (parameter.upper - parameter.lower) / \
+    #                    (fidelity_space_granularity - 1)
+    #
+    #         grid_points = np.arange(
+    #             start=parameter.lower, stop=parameter.upper + eps, step=step
+    #         )
+    #
+    #         if isinstance(parameter, CS.UniformIntegerHyperparameter):
+    #             grid_points = np.ceil(grid_points).astype(int)
+    #
+    #         grid_points = np.clip(grid_points, a_min=None, a_max=parameter.upper)
+    #         fidelity_grid.append(grid_points)
+    #     fidelity_grid = list(itertools.product(*fidelity_grid))
+    #     fidelities = []
+    #     for i, fidelity in enumerate(fidelity_grid):
+    #         dummy_fidelity = benchmark.f_cs.sample_configuration()
+    #         config = fidelity[-1]
+    #         for j, parameter in enumerate(benchmark.f_cs.get_hyperparameters()):
+    #             dummy_fidelity[parameter.name] = fidelity[j]
+    #         fidelities.append(dummy_fidelity)
+    #
+    #     # all combinations of evaluations to be made
+    #     evaluations = list(itertools.product(*([configs, fidelities])))
+    #
+    #     for i in range(len(evaluations)):
+    #         print("Evaluating {} out of {}".format(
+    #             i + 1, n_configs * fidelity_space_granularity ** 2
+    #         ), end='\r')
+    #         config, fidelity = evaluations[i]
+    #         results[task_id].append(benchmark.objective(config, fidelity))
+    #
+    #     print(results[task_id][0])
+    #     print(results[task_id][-1])
+    #     print()
+    #     print("Time taken since beginning: {:<.5f} seconds".format(time.time() - start))
--- run_benchmark_joblib.py
+++ run_benchmark_joblib.py
@@ -10,8 +10,6 @@ from joblib import Parallel, delayed
 
 if __name__ == "__main__":
 
-    print(193.20521, "==>", 98.32635, end='\n')
-
     num_workers = int(sys.argv[1])
     print("Executing with {} worker(s)...".format(num_workers))
 
@@ -19,7 +17,7 @@ if __name__ == "__main__":
         3,       # kr-vs-kp
         # 168335,  # MiniBooNE   ### why failed???
         # 168331,  # volkert
-        # 31,      # credit-g
+        31,      # credit-g
         # 9977     # nomao
     ]
 
@@ -27,70 +25,125 @@ if __name__ == "__main__":
     n_configs = 5
     fidelity_space_granularity = 5
 
-    results = {}
+    results = []
 
-    start = time.time()
-    # for task_id in task_ids:
-    def p1(task_id):
+    benchmark = RandomForestBenchmark(task_id=task_ids[0], seed=seed)
+
+    # Create list of configs
+    configs = benchmark.get_config(size=n_configs)
+
+    # Create list of fidelities
+    fidelity_grid = []
+    eps = 1e-10
+    for i, parameter in enumerate(benchmark.f_cs.get_hyperparameters()):
+        if isinstance(parameter, CS.UniformIntegerHyperparameter) and \
+                (parameter.upper - parameter.lower) < fidelity_space_granularity:
+            step = 1
+        else:
+            step = (parameter.upper - parameter.lower) / \
+                   (fidelity_space_granularity - 1)
+
+        grid_points = np.arange(
+            start=parameter.lower, stop=parameter.upper + eps, step=step
+        )
+
+        if isinstance(parameter, CS.UniformIntegerHyperparameter):
+            grid_points = np.ceil(grid_points).astype(int)
+
+        grid_points = np.clip(grid_points, a_min=None, a_max=parameter.upper)
+        fidelity_grid.append(grid_points)
+    fidelity_grid = list(itertools.product(*fidelity_grid))
+    fidelities = []
+    for i, fidelity in enumerate(fidelity_grid):
+        dummy_fidelity = benchmark.f_cs.sample_configuration()
+        config = fidelity[-1]
+        for j, parameter in enumerate(benchmark.f_cs.get_hyperparameters()):
+            dummy_fidelity[parameter.name] = fidelity[j]
+        fidelities.append(dummy_fidelity)
+
+    # all combinations of evaluations to be made
+    evaluations = list(itertools.product(*([task_ids, configs, fidelities])))
+
+    def loop_fn(evaluation):
+        # print("Evaluating {} out of {}".format(
+        #     i + 1, len(task_ids) * n_configs * fidelity_space_granularity ** 2
+        # ), end='\r')
+        task_id, config, fidelity = evaluation
         benchmark = RandomForestBenchmark(task_id=task_id, seed=seed)
         benchmark.load_data_automl()
-        fidelity_grid = []
-        eps = 1e-10
-        results[task_id] = []
-        print("Beginning task_id={}\n{}\n".format(task_id, '=' * 25))
+        return task_id, benchmark.objective(config, fidelity)
 
-        # Create list of configs
-        configs = benchmark.get_config(size=n_configs)
-
-        # Create list of fidelities
-        for i, parameter in enumerate(benchmark.f_cs.get_hyperparameters()):
-            if isinstance(parameter, CS.UniformIntegerHyperparameter) and \
-                    (parameter.upper - parameter.lower) < fidelity_space_granularity:
-                step = 1
-            else:
-                step = (parameter.upper - parameter.lower) / \
-                       (fidelity_space_granularity - 1)
-
-            grid_points = np.arange(
-                start=parameter.lower, stop=parameter.upper + eps, step=step
-            )
-
-            if isinstance(parameter, CS.UniformIntegerHyperparameter):
-                grid_points = np.ceil(grid_points).astype(int)
-
-            grid_points = np.clip(grid_points, a_min=None, a_max=parameter.upper)
-            fidelity_grid.append(grid_points)
-        fidelity_grid = list(itertools.product(*fidelity_grid))
-        fidelities = []
-        for i, fidelity in enumerate(fidelity_grid):
-            dummy_fidelity = benchmark.f_cs.sample_configuration()
-            config = fidelity[-1]
-            for j, parameter in enumerate(benchmark.f_cs.get_hyperparameters()):
-                dummy_fidelity[parameter.name] = fidelity[j]
-            fidelities.append(dummy_fidelity)
-
-        # all combinations of evaluations to be made
-        evaluations = list(itertools.product(*([configs, fidelities])))
-
-        with Parallel(n_jobs=num_workers) as parallel2:
-            results[task_id] = parallel2(
-                delayed(benchmark.objective)(evaluations[idx][0], evaluations[idx][1])
-                for idx in range(len(evaluations))
-            )
-        return results
-
-    # parallelizes over tasks
-    with Parallel(n_jobs=num_workers) as parallel1:
-        output = parallel1(delayed(p1)(task_id) for task_id in task_ids)
+    start = time.time()
+    with Parallel(n_jobs=num_workers) as parallel:
+        output = parallel(delayed(loop_fn)(evaluation) for evaluation in evaluations)
 
     for i in range(len(output)):
-        results.update(output[i])
+        results.append(output[i])
 
-    end =time.time()
-    print()
-    for task_id in task_ids:
-        print("\nTask_id: {}\n{}".format(task_id, "-"*25))
-        print(results[task_id][0])
-        print(results[task_id][-1])
-    print()
-    print("Time taken: {:<.5f} seconds".format(end- start))
+    print("Time taken since beginning: {:<.5f} seconds".format(time.time() - start))
+
+    # start = time.time()
+    # # for task_id in task_ids:
+    # def p1(task_id):
+    #     benchmark = RandomForestBenchmark(task_id=task_id, seed=seed)
+    #     benchmark.load_data_automl()
+    #     fidelity_grid = []
+    #     eps = 1e-10
+    #     results[task_id] = []
+    #     print("Beginning task_id={}\n{}\n".format(task_id, '=' * 25))
+    #
+    #     # Create list of configs
+    #     configs = benchmark.get_config(size=n_configs)
+    #
+    #     # Create list of fidelities
+    #     for i, parameter in enumerate(benchmark.f_cs.get_hyperparameters()):
+    #         if isinstance(parameter, CS.UniformIntegerHyperparameter) and \
+    #                 (parameter.upper - parameter.lower) < fidelity_space_granularity:
+    #             step = 1
+    #         else:
+    #             step = (parameter.upper - parameter.lower) / \
+    #                    (fidelity_space_granularity - 1)
+    #
+    #         grid_points = np.arange(
+    #             start=parameter.lower, stop=parameter.upper + eps, step=step
+    #         )
+    #
+    #         if isinstance(parameter, CS.UniformIntegerHyperparameter):
+    #             grid_points = np.ceil(grid_points).astype(int)
+    #
+    #         grid_points = np.clip(grid_points, a_min=None, a_max=parameter.upper)
+    #         fidelity_grid.append(grid_points)
+    #     fidelity_grid = list(itertools.product(*fidelity_grid))
+    #     fidelities = []
+    #     for i, fidelity in enumerate(fidelity_grid):
+    #         dummy_fidelity = benchmark.f_cs.sample_configuration()
+    #         config = fidelity[-1]
+    #         for j, parameter in enumerate(benchmark.f_cs.get_hyperparameters()):
+    #             dummy_fidelity[parameter.name] = fidelity[j]
+    #         fidelities.append(dummy_fidelity)
+    #
+    #     # all combinations of evaluations to be made
+    #     evaluations = list(itertools.product(*([configs, fidelities])))
+    #
+    #     with Parallel(n_jobs=num_workers) as parallel2:
+    #         results[task_id] = parallel2(
+    #             delayed(benchmark.objective)(evaluations[idx][0], evaluations[idx][1])
+    #             for idx in range(len(evaluations))
+    #         )
+    #     return results
+    #
+    # # parallelizes over tasks
+    # with Parallel(n_jobs=num_workers) as parallel1:
+    #     output = parallel1(delayed(p1)(task_id) for task_id in task_ids)
+    #
+    # for i in range(len(output)):
+    #     results.update(output[i])
+    #
+    # end =time.time()
+    # print()
+    # for task_id in task_ids:
+    #     print("\nTask_id: {}\n{}".format(task_id, "-"*25))
+    #     print(results[task_id][0])
+    #     print(results[task_id][-1])
+    # print()
+    # print("Time taken: {:<.5f} seconds".format(end- start))
