90da221e523bd6befbbd5c97cc4a72cf67ce47f9
neeratyoy <neeratyoy@gmail.com>
2021-07-26 22:05:17 +0200

    Testing script fix

--- scripts/test_benchmarks.sh
+++ scripts/test_benchmarks.sh
@@ -7,7 +7,7 @@ fidelity=1
 
 path="tmp_dump/test/"$model"/"$fidelity"/benchmark/"
 path="nemo_dump/"$model"/"$fidelity"/benchmark/"
-path="/work/ws/nemo/fr_nm217-hpobench-0/full/"$model$/$fidelity"/benchmark/"
+path="/work/ws/nemo/fr_nm217-hpobench-0/full/"$model/$fidelity"/benchmark/"
 
 for tid in "${tids[@]}"
 do
