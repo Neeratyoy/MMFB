d7e900a7ca798c566ad699003897aca5cb9aef78
neeratyoy <neeratyoy@gmail.com>
2021-08-02 22:13:05 +0200

    Including thread limit env. var. in nemo config

--- scripts/nemo/config.sh
+++ scripts/nemo/config.sh
@@ -4,3 +4,7 @@
 #export DASK_DISTRIBUTED__SCHEDULER__ALLOWED_FAILURES=10
 export DASK_DISTRIBUTED__COMM__TIMEOUTS__CONNECT=100
 export DASK_DISTRIBUTED__COMM__RETRY__COUNT=2
+
+# important for sklearn models to run on single CPU cores
+export MKL_NUM_THREADS=1
+export OMP_NUM_THREADS=1
