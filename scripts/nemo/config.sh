b2e0545f4c738278b75734dec6113f696a2a581b
neeratyoy <neeratyoy@gmail.com>
2021-08-03 17:02:37 +0200

    Allowing worker stealing for Dask

--- scripts/nemo/config.sh
+++ scripts/nemo/config.sh
@@ -5,6 +5,9 @@
 export DASK_DISTRIBUTED__COMM__TIMEOUTS__CONNECT=100
 export DASK_DISTRIBUTED__COMM__RETRY__COUNT=2
 
+# allows Dask schedulers to redistribute worker load
+export DASK_DISTRIBUTED__SCHEDULER__WORK_STEALING=True
+
 # important for sklearn models to run on single CPU cores
 export MKL_NUM_THREADS=1
 export OMP_NUM_THREADS=1
