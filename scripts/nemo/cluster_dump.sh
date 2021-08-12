db36a95d8fab480305086c91254ee532434152d6
neeratyoy <neeratyoy@gmail.com>
2021-08-12 19:28:42 +0200

    Scripting to have the option to run a small cluster per task per space

--- scripts/nemo/cluster_dump.sh
+++ scripts/nemo/cluster_dump.sh
@@ -3,12 +3,16 @@
 codedir=$HOME'/Thesis/code/MMFB'
 
 model=$1
+nworkers=$2
+scheduler=$3
+ppn=20
+ajobs=`expr $nworkers / $ppn`
 
 echo "Submitting scheduler"
-msub $codedir/scripts/nemo/create_scheduler.sh scheduler_$model
+msub $codedir/scripts/nemo/create_scheduler.sh $scheduler
 
 echo -e "\nSubmitting collator"
 python scripts/nemo/submit_collator.py --space $model
 
 echo -e "\nSubmitting first 100 workers"
-msub -t 1-5 $codedir/scripts/nemo/arrayjob.moab scheduler_$model
+msub -t 1-$ajobs $codedir/scripts/nemo/arrayjob.moab $scheduler
--- scripts/nemo/submit_jobs.py
+++ scripts/nemo/submit_jobs.py
@@ -3,7 +3,7 @@ import time
 import argparse
 import subprocess
 
-from utils.util import all_task_ids_by_in_mem_size
+from utils.util import all_task_ids_by_in_mem_size, load_yaml_args, dump_yaml_args
 
 
 def edit_submit_script(codedir, space, exp_type, task_id):
@@ -24,6 +24,16 @@ def edit_submit_script(codedir, space, exp_type, task_id):
     return
 
 
+def edit_args_scheduler(codedir, space, task_id, scheduler):
+    base_path = "arguments/nemo/full/{}/".format(space)
+    config = load_yaml_args(os.path.join(codedir, base_path, "args_{}.yaml".format(task_id)))
+    _sch = config.scheduler_file.split('/')
+    _sch[-1] = "{}.json".format(scheduler)
+    config.scheduler_file = "/".join(_sch)
+    dump_yaml_args(dict(config), os.path.join(codedir, base_path, "args_{}.yaml".format(task_id)))
+    return
+
+
 def input_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--tasks", default=None, nargs="+", type=int)
@@ -31,6 +41,7 @@ def input_args():
     parser.add_argument("--exp_type", default="full", type=str, choices=["full", "toy"])
     parser.add_argument("--codedir", default="/home/fr/fr_fr/fr_nm217/Thesis/code/MMFB", type=str)
     parser.add_argument("--sleep", default=5, type=float)
+    parser.add_argument("--scheduler", default=None, type=str, help="scheduler file name")
     args = parser.parse_args()
     return args
 
@@ -45,5 +56,7 @@ if __name__ == "__main__":
     print(script_path)
     for task_id in task_ids:
         edit_submit_script(args.codedir, args.space, args.exp_type, task_id)
+        if args.scheduler is not None:
+            edit_args_scheduler(args.codedir, args.space, task_id, args.scheduler)
         subprocess.call(["msub", script_path])
         time.sleep(args.sleep)
