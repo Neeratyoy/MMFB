d35c62fb3f34d59f6fbb8b56838f9e8f160844f5
neeratyoy <neeratyoy@gmail.com>
2022-01-06 13:07:17 +0530

    Updating benchmark testing script

--- scripts/test_benchmarks.sh
+++ scripts/test_benchmarks.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-declare -a tids=(10101 53 146818)
+declare -a tids=(10101 53 146818 146821 9952 146822 31 3917 168912 3 167119 12 146212 168911 9981 167120 14965 146606 7592 9977)
 
 model=$1
 fidelity=1
@@ -8,7 +8,7 @@ cores=$2
 
 #path="tmp_dump/test/"$model"/"$fidelity"/benchmark/"
 #path="nemo_dump/TabularData/"$model"/"$fidelity"/benchmark/"
-path="/work/ws/nemo/fr_nm217-hpobench-0/full/"$model/$fidelity"/benchmark/"
+path="/work/ws/nemo/fr_nm1068-hpobench-0/full/"$model/$fidelity"/benchmark/"
 
 for tid in "${tids[@]}"
 do
